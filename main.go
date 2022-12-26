46f86755fed84c092a91bb60af78ad2c14ab153c
Aalekh Singh <aalekhsingh48@gmail.com>
2022-12-26 09:30:56 +0530

    Improved Interface

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"net/http"
 )
 
+type Form struct {
+	Name              string
+	Phone_No          string
+	Age               string
+	Highest_Education string
+	Tweleve_Marks     string
+	Catogary          string
+}
+
 func hellohandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "Error 404 Page not Found", http.StatusNotFound)
@@ -24,10 +33,22 @@ func formhandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Post Request Successfull \n")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name= %s \n", name)
-	fmt.Fprintf(w, "Phone No= %s \n", address)
+	var data Form
+	data.Name = r.FormValue("name")
+	data.Phone_No = r.FormValue("address")
+	data.Age = r.FormValue("age")
+	data.Catogary = r.FormValue("catogry")
+	data.Tweleve_Marks = r.FormValue("marks")
+	data.Highest_Education = r.FormValue("heducation")
+
+	fmt.Fprintf(w, "Name= %s \n", data.Name)
+	fmt.Fprintf(w, "Phone No= %s \n", data.Phone_No)
+	fmt.Fprintf(w, "Age= %s \n", data.Age)
+	fmt.Fprintf(w, "Caetgory= %s \n", data.Catogary)
+	fmt.Fprintf(w, "Tweleve Marks= %s \n", data.Tweleve_Marks)
+	fmt.Fprintf(w, "Highest Education= %s \n", data.Highest_Education)
+	// jsondata, _ := json.Marshal(data)
+	// fmt.Println(jsondata)
 
 }
 
